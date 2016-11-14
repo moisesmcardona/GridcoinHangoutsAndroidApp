815ca317796c8a58123b00046539d7e42f0f26ee
moisesmcardona <moises@moises-studios.com>
2016-11-14 16:28:09 -0400

    Initial commit

---
+++ app/src/main/java/mcr/gridcoin/hangouts/Config.java
@@ -0,0 +1,10 @@
+package mcr.gridcoin.hangouts;
+
+/**
+ * Created by echessa on 6/17/15.
+ */
+public class Config {
+
+    public static final String CLIENT_ID = "API_KEY";
+    public static final String API_URL = "https://api.soundcloud.com";
+}
